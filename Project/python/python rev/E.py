34208ad52d4689bafade7178df3713122712b233
Anik8 <124755066+Aniket9657@users.noreply.github.com>
2026-04-15 08:14:17 +0530

    Delete Project/python/python rev directory

--- Project/python/python rev/E.py
+++
@@ -1,4 +0,0 @@
-#zip file open 
-import zipfile 
-archive = zipfile.ZipFile('D:\python-3.14-docs-html.zip','r')
-imgfile = archive.open
--- Project/python/python rev/Pro_3.py
+++
@@ -1,4 +0,0 @@
-l=dict([("sape",3421),("guido",7865)])
-print(l)
-m=dict(sape=4139, guido=4127, jack=4098)
-print(m)

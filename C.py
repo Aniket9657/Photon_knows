c613ad3761255b1175cb71ca782311ac23691a18
Anik8 <124755066+Aniket9657@users.noreply.github.com>
2026-04-15 08:13:01 +0530

    Delete C.py

--- C.py
+++
@@ -1,9 +0,0 @@
-def palin_check(word):
-    for w in range(len(word)//2):
-        if word[w] != word[len(word)-1-w]:
-            return False
-    return True
-        
-word ="AWW"
-print(palin_check(word))
- 
